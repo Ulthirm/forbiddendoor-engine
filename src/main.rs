2be91938b5e7e9614799414251be919227405479
Ulthirm <Ulthirm@gmail.com>
2024-07-18 21:46:56 -0500

    eh just finishing up logging stuff

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
+use tracing::{debug, info, warn};
+use tracing_subscriber::FmtSubscriber;
+
+// Modules avoid declaring with the USEs please
 mod config;
 
 #[tokio::main]
@@ -6,7 +10,21 @@ async fn main() {
     // No point in diagnosing flat crashes from outside the machine
     println!("Initializing Engine");
 
-    // using the logging config stuff
+    // get the logging config
+    // We actually use tracing but this is legacy
     let logging_config = config::get_logging_config();
     println!("Logging level: {:?}", logging_config);
+
+    // Set up the tracing subscriber here
+    // This is like logging but different. 
+    // Some of the crates we use, use tracing so we're stuck with it.
+    // Don't ask me to explain it. I dont really understand it very well myself
+    let subscriber = FmtSubscriber::builder()
+        .with_max_level(logging_config)
+        .finish();
+
+    tracing::subscriber::set_global_default(subscriber).expect("setting default subscriber failed");
+
+
+    info!("test");
 }
